0cdad8def8d1fedba5dd87f3fdf619fc71f85181
Hyunseok Lim <hyunseoki@outlook.kr>
2023-02-03 00:30:47 +0900

    dataset.json 불필요한 요소 삭제

--- utils.py
+++ utils.py
@@ -22,26 +22,7 @@ def get_identifiers_from_splitted_files(folder: str):
     return uniques
 
 def generate_dataset_json(output_file: str, imagesTr_dir: str, imagesTs_dir: str, modalities: Tuple,
-                          labels: dict, dataset_name: str, sort_keys=True, license: str = "hands off!", dataset_description: str = "",
-                          dataset_reference="", dataset_release='0.0'):
-    """
-    :param output_file: This needs to be the full path to the dataset.json you intend to write, so
-    output_file='DATASET_PATH/dataset.json' where the folder DATASET_PATH points to is the one with the
-    imagesTr and labelsTr subfolders
-    :param imagesTr_dir: path to the imagesTr folder of that dataset
-    :param imagesTs_dir: path to the imagesTs folder of that dataset. Can be None
-    :param modalities: tuple of strings with modality names. must be in the same order as the images (first entry
-    corresponds to _0000.nii.gz, etc). Example: ('T1', 'T2', 'FLAIR').
-    :param labels: dict with int->str (key->value) mapping the label IDs to label names. Note that 0 is always
-    supposed to be background! Example: {0: 'background', 1: 'edema', 2: 'enhancing tumor'}
-    :param dataset_name: The name of the dataset. Can be anything you want
-    :param sort_keys: In order to sort or not, the keys in dataset.json
-    :param license:
-    :param dataset_description:
-    :param dataset_reference: website of the dataset, if available
-    :param dataset_release:
-    :return:
-    """
+                          labels: dict, dataset_name: str, sort_keys=True, dataset_description: str = ""):
     train_identifiers = get_identifiers_from_splitted_files(imagesTr_dir)
 
     if imagesTs_dir is not None:
@@ -52,10 +33,6 @@ def generate_dataset_json(output_file: str, imagesTr_dir: str, imagesTs_dir: str
     json_dict = {}
     json_dict['name'] = dataset_name
     json_dict['description'] = dataset_description
-    json_dict['tensorImageSize'] = "4D"
-    json_dict['reference'] = dataset_reference
-    json_dict['licence'] = license
-    json_dict['release'] = dataset_release
     json_dict['modality'] = {str(i): modalities[i] for i in range(len(modalities))}
     json_dict['labels'] = {str(i): labels[i] for i in labels.keys()}
 

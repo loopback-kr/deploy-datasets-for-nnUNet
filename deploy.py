7fc031c1f61534f57cb260b3468475b920c3dda5
Hyunseok Lim <hyunseoki@outlook.kr>
2023-02-03 00:30:22 +0900

    main 파일 구현

--- deploy.py
+++ deploy.py
@@ -1,27 +1,62 @@
 from lib import *
-from utils import generate_dataset_json
 
+# For ISLES
+DATASET_ROOT_DIR = 'Rawdata/Task700_ISLES_preprocessed_ADC_DWI_FLAIR'
+DATASET_LOADER = 'loader_ISLES22_preprocessed'
+# For BraTS
 DATASET_ROOT_DIR = 'Rawdata/BraTS2021_Training_Data'
 DATASET_LOADER = 'loader_BraTS2021'
+DATASET_NAME = 'BraTS2021'
+
 DST_DIR = './nnUNet'
-TASK_NAME = 'Task670_BraTS2021_T1_1Necrotic'
-TASK_NAME = 'Task901'
-REMAIN_ORIGIN_LABEL = 1
-REMOVE_LABELS = [2,4]
-REMOVE_MODALITIES = ['t1', 't1ce', 't2']
-TASK_IDS = [
-    [670, 671, 672, 673],
-    [670, 671, 672, 673],
-    [670, 671, 672, 673],
+# TASK_IDS = range(670, 673)
+# TASK_IDS = range(673, 676)
+# TASK_IDS = range(676, 679)
+TASK_IDS = range(679, 682)
+EXCLUDED_MODS = [
+    ['t1', 't1ce', 't2'],
+    ['t1', 't1ce', 't2'],
+    ['t1', 't1ce', 't2'],
+]
+EXCLUDED_LABELS = [
+    [0,2,4],
+    [0,1,4],
+    [0,1,2],
 ]
 
 
-if __name__ == '__main__':
+TASK_NAMES = {
+    id: f'Task{id}-{DATASET_NAME}-{"_".join(list(set(find_loader(DATASET_LOADER).MODALITIES) - set(excluded_mods)))}-{"_".join([find_loader(DATASET_LOADER).LBL_LEGENDS[i] for i in [find_loader(DATASET_LOADER).LABELS.index(i) for i in list(set(find_loader(DATASET_LOADER).LABELS) - set(excluded_lbls))]])}'
+    
+    for id, excluded_mods, excluded_lbls in zip(
+        TASK_IDS,
+        EXCLUDED_MODS,
+        EXCLUDED_LABELS,
+    )
+}
 
-    loader = find_loader(DATASET_LOADER)(dataset_dir=join(DST_DIR, TASK_NAME))
-    source_paths = sorted(list(iglob(join(DATASET_ROOT_DIR, '**', '*.nii.gz'), recursive=True)))[:10]
-    dest_paths = [join(DST_DIR, TASK_NAME, 'imagesTr', basename(path)) for path in source_paths]
-    run_multiproc(loader.copy, source_paths, dest_paths, desc='Deploying dataset', num_processes=1)
-    generate_dataset_json(join(DST_DIR, TASK_NAME, 'dataset.json'), join(DST_DIR, TASK_NAME, 'imagesTr'), None, loader.MODALITIES, {label: legend for label, legend in zip(loader.LABELS, loader.LEGENDS_SHORT)}, dataset_name=TASK_NAME)
-    print('Total num of imagesTr: ', len(glob(join(DST_DIR, TASK_NAME, 'imagesTr', '*.nii.gz'))))
-    print('Total num of labelsTr: ', len(glob(join(DST_DIR, TASK_NAME, 'labelsTr', '*.nii.gz'))))
+
+if __name__ == '__main__':
+    
+    for idx, (task_id, task_name) in enumerate(TASK_NAMES.items()):
+    
+        loader = find_loader(DATASET_LOADER)(
+            dataset_dir=join(DST_DIR, task_name),
+            overwrite=True,
+            excluded_mods=EXCLUDED_MODS[idx],
+            excluded_labels=EXCLUDED_LABELS[idx]
+        )
+        
+        source_paths = sorted(list(iglob(join(DATASET_ROOT_DIR, '**', '*.nii.gz'), recursive=True)))
+        dest_paths = [join(DST_DIR, task_name, 'imagesTr', basename(path)) for path in source_paths]
+        
+        run_multiproc(loader.copy, source_paths, dest_paths, desc='Deploying dataset', num_processes=1)
+        loader.generate_dataset_json(
+            join(DST_DIR, task_name, 'dataset.json'),
+            join(DST_DIR, task_name, 'imagesTr'),
+            None,
+            dataset_name=task_name
+        )
+        
+        print('Total num of imagesTr: ', len(glob(join(DST_DIR, task_name, 'imagesTr', '*.nii.gz'))))
+        print('Total num of labelsTr: ', len(glob(join(DST_DIR, task_name, 'labelsTr', '*.nii.gz'))))
